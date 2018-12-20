066b54850f7fa8b00d888cf37a4130c03033677f
yulicaa <yulidcahya99@gmail.com>
2018-12-20 20:01:56 +0700

    menambahkan database dan class koneksi

---
+++ src/aplikasipendaftaran/Koneksi.java
@@ -0,0 +1,26 @@
+package aplikasipendaftaran;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+/**
+ *
+ * @author Yulica
+ */
+public class Koneksi {
+    public static Connection con;
+    public static Statement stm;
+    public static void main(String args[]){
+        try {
+            String url ="jdbc:mysql://localhost/aplikasipendaftaran";
+            String user="root";
+            String pass="";
+            Class.forName("com.mysql.jdbc.Driver");
+            con =DriverManager.getConnection(url,user,pass);
+            stm = con.createStatement();
+            System.out.println("koneksi berhasil;");
+        } catch (Exception e) {
+            System.err.println("koneksi gagal" +e.getMessage());
+        }
+    }
+}
